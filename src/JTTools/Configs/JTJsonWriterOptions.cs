a185ce942bac5a8e0af35f80ffd8721e2b4bcd1e
SmallChi(Koike) <564952747@qq.com>
2020-04-07 18:14:33 +0800

    完善各个协议的功能

---
+++ src/JTTools/Configs/JTJsonWriterOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace JTTools.Configs
+{
+    public static class JTJsonWriterOptions
+    {
+        /// <summary>
+        /// 汉字编码
+        /// </summary>
+        public readonly static JsonWriterOptions Instance = new System.Text.Json.JsonWriterOptions
+        {
+            Indented = true,
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+        };  
+    }
+}

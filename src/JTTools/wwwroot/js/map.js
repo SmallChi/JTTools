5945b8bcbc8c39b90cf5e1aef6cf057d5bda7337
SmallChi(Koike) <564952747@qq.com>
2021-04-13 18:24:20 +0800

    增加地图批量打点

--- src/JTTools/wwwroot/js/map.js
+++ src/JTTools/wwwroot/js/map.js
@@ -20,3 +20,22 @@ function createMarker(gcj02_lng,gcj02_lat) {
     window.map.add(marker);
     window.map.setFitView(marker);
 }
+
+function createMarkers(arr) {
+    console.debug("Markers", arr);
+    var lastMarker = null;
+    for (var i = 0; i < arr.length; i++) {
+        var item = arr[i];
+        var marker = new AMap.Marker({
+            position: new AMap.LngLat(item[0], item[1]), // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+            icon: new AMap.Icon({
+                imageSize: new AMap.Size(36, 36),
+            })
+        });
+        if ((arr.length - 1) == i) {
+            lastMarker = marker;
+        }
+        window.map.add(marker);
+    }
+    window.map.setFitView(lastMarker);
+}
